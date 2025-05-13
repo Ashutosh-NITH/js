497809c6a04f03b1d86f9644f891f65bf5a8b441
Ashutosh <ashutosh.93343.kumar@gmail.com>
2025-05-13 20:03:40 +0000

    commit done

---
+++ basic/var.js
@@ -0,0 +1,11 @@
+const account_id = 655757
+let account_email = "ashukyakartahai@gmail.com"
+var account_name = "Ashutosh"
+let phone = 9229258016
+let accountstate
+
+console.table([account_id , account_name , account_email , phone, accountstate])
+
+//note :
+/* do not use 
+var */
